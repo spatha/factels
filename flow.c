1043aa60ff6e8ccf156aa7a30b1d36d05dda51ff
Yury Vorobyov <teupollam@gmail.com>
2012-11-25 16:37:33 +0000

    fine

--- flow.c
+++ flow.c
@@ -27,6 +27,10 @@ inline double field ( double r ) {
 	return epsilon * ( pow(r,-14) - pow(r,-7) );
 }
 
+inline double force ( double r ) {
+	return epsilon * ( -14 * pow(r,-15) - (-7) * pow(r,-8) );
+}
+
 bool isNear(int i, int j)
 {
 	if ( range(i,j) < cutRad ) {
@@ -37,8 +41,10 @@ bool isNear(int i, int j)
 int init()
 {
 	for(i=0;i<N;i++) {
-		acc[i]=0;
-		vel[i]=0;
+		ax[i]=0;
+		ay[i]=0;
+		vx[i]=0;
+		vy[i]=0;
 		x[i]=sqrt(i)/sqrt(N);
 		y[i]=1-sqr(i/N);
 //		ord[i]=rand()/double(RAND_MAX);
@@ -48,13 +54,14 @@ int init()
 
 int domove()
 {
-	double U=0;
+	double Fx=0,Fy=0;
 	double r=1;
 	for(i=0; i<N; i++) {
 		for(j=0; j<N; j++) {
 			if (isNear(i,j)) {
 				r = range(i,j);
-				U += field(r);
+				Fx += force(r) * (x[i]-x[j])/range(i,j);
+				Fy += force(r) * (y[i]-y[j])/range(i,j);
 				
 			}
 		}
@@ -62,8 +69,8 @@ int domove()
 		y[i]+=dt*vy[i];
 		vx[i]+=dt*ax[i];
 		vy[i]+=dt*ay[i];
-		ax[i]= DUx;
-		ay[i]= DUy;
+		ax[i]= Fx;
+		ay[i]= Fy;
 	}
 	return 0;
 }
